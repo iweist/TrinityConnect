bede3b75a394213d64a84772a39f5eff3102cd6f
iweist <ian.weist@trincoll.edu>
2015-10-08 10:59:14 -0400

    First Version

---
+++ CriminalIntent/app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/Crime.java
@@ -0,0 +1,50 @@
+package criminalintent.android.bignerdranch.com.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by owner on 10/4/2015.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+}
