15566cbbd2393402541b5dbc1f4396a733ec55dd
iweist <ian.weist@trincoll.edu>
2015-11-15 23:00:55 -0500

    Start to TrinityConnect. Work by: Ian + Adam

---
+++ TrinityConnect/app/src/main/java/trinityconnect/android/bignerdranch/com/trinityconnect/EventListFragment.java
@@ -0,0 +1,10 @@
+package trinityconnect.android.bignerdranch.com.trinityconnect;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by owner on 11/15/2015.
+ */
+public class EventListFragment extends Fragment {
+
+}
